c27ce324f8dd4c9d508b792c7ef866fbf39ddf4a
sanchit-157 <sanchit1659.becse24@chitkara.edu.in>
2025-02-12 11:55:35 +0530

    Solution to Bubble Sort Implementation

--- Bubble Sort Implementation@@67a4d9012ee20af0252bc6cb/code.c
+++ Bubble Sort Implementation@@67a4d9012ee20af0252bc6cb/code.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 void bubbleSort(int arr[], int n) {
     int temp;
@@ -18,7 +19,13 @@ int main() {
     printf("Enter the number of integers: ");
     scanf("%d", &n);
 
-    int arr[n];
+    int* arr = (int*)malloc(n * sizeof(int));
+
+    if (arr == NULL) {
+        printf("Memory allocation failed.\n");
+        return 1;
+    }
+
     printf("Enter %d integers:\n", n);
     for (int i = 0; i < n; i++) {
         scanf("%d", &arr[i]);
@@ -32,5 +39,6 @@ int main() {
     }
     printf("\n");
 
+    free(arr);
     return 0;
 }
